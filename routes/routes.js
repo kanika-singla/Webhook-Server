6395ad2011cbec1f1167282e3a99fa4b4e9d38e3
kanika-singla <kanika-singla@outlook.com>
2020-11-15 16:02:57 +0530

    route for triggering webhook

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,22 @@
 module.exports = function(app) {
 
     app.get("/", function(request, response) {
-        response.send({ msg: "Welcome!"});
+        response.send({
+            msg: "Welcome!"
+        });
     });
 
     //POST request to register webhook 
     app.post("/api/webhooks", async function(request, response) {
         response.send({
             msg: "Webhook registered!"
+        });
+    });
+
+    //POST request to trigger webhooks
+    app.post("/api/webhooks/test", async function(request, response) {
+        response.send({
+            msg: "Webhook triggered!"
         });
     });
 
